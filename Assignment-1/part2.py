ce3e727e3e302fb100c5af7291b13d294198da28
Arthur Viegas Eguia <95381448+Arthur-Viegas-Eguia@users.noreply.github.com>
2022-03-22 18:17:22 -0500

    Delete part2.py

--- Assignment-1/part2.py
+++
@@ -1,9 +0,0 @@
-def pairsThatEqualSum(inputArray: list, targetSum: int) -> list:
-    toSum = {}
-    pairsThatEqualSum = []
-    for item in inputArray:
-        if item in toSum:
-            pairsThatEqualSum.append((item, toSum[item]))
-        else:
-            toSum[targetSum - item] = item
-    return pairsThatEqualSum
