acb38b89474b664b10855497a3f2b0340533f96a
Arthur Viegas Eguia <95381448+Arthur-Viegas-Eguia@users.noreply.github.com>
2022-06-14 17:36:36 -0500

    Create OrganizationStructureExercise2.java

---
+++ Assignment-2/Part-1/OrganizationStructureExercise2.java
@@ -0,0 +1,51 @@
+import java.util.List;
+import java.util.LinkedList;
+public class OrganizationStructureExercise1 {
+  public Employee ceo;
+
+  public static class Employee {
+      public String name;
+      public String title;
+      public List<Employee> directReports;
+  }
+  private void printNumLevels(){
+    LinkedList<Employee> queue = new LinkedList<>();
+    LinkedList <Employee> nextLevel = new LinkedList<>();
+    int count = 0;
+    queue.add(ceo);
+    while (queue.peek() != null){
+      for(int i = 0; i < queue.size(); i ++){
+        if(queue.get(i).directReports != null){
+          for(Employee reports : queue.get(i).directReports){
+            nextLevel.add(reports);
+          }
+        }
+      }
+        count++;
+        queue = nextLevel;
+        nextLevel = new LinkedList<>();
+      }
+    System.out.println(count);
+  }
+  private void printLevelByLevel(){
+    LinkedList<Employee> queue = new LinkedList<>();
+    LinkedList <Employee> nextLevel = new LinkedList<>();
+    queue.add(ceo);
+    while (queue.peek() != null){
+      for(int i = 0; i < queue.size(); i ++){
+        if(queue.get(i).directReports != null){
+          for(Employee reports : queue.get(i).directReports){
+            nextLevel.add(reports);
+          }
+        }
+      }
+        while(!queue.isEmpty()){
+          Employee employee = queue.removeFirst();
+          System.out.println("Name: "+employee.name+" Title: "+employee.title);
+        }
+        System.out.print("\n");
+        queue = nextLevel;
+        nextLevel = new LinkedList<>();
+      }
+  }
+}
