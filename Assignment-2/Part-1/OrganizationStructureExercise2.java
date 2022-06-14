4e82a591aa5fd98aaedebc096513c36facd1b3d3
Arthur Viegas Eguia <95381448+Arthur-Viegas-Eguia@users.noreply.github.com>
2022-06-14 17:41:39 -0500

    Update and rename OrganizationStructureExercise1.java to OrganizationStructureExercise2.java

--- Assignment-2/Part-1/OrganizationStructureExercise2.java
+++ Assignment-2/Part-1/OrganizationStructureExercise2.java
@@ -8,25 +8,6 @@ public class OrganizationStructureExercise1 {
       public String title;
       public List<Employee> directReports;
   }
-  private void printNumLevels(){
-    LinkedList<Employee> queue = new LinkedList<>();
-    LinkedList <Employee> nextLevel = new LinkedList<>();
-    int count = 0;
-    queue.add(ceo);
-    while (queue.peek() != null){
-      for(int i = 0; i < queue.size(); i ++){
-        if(queue.get(i).directReports != null){
-          for(Employee reports : queue.get(i).directReports){
-            nextLevel.add(reports);
-          }
-        }
-      }
-        count++;
-        queue = nextLevel;
-        nextLevel = new LinkedList<>();
-      }
-    System.out.println(count);
-  }
   private void printLevelByLevel(){
     LinkedList<Employee> queue = new LinkedList<>();
     LinkedList <Employee> nextLevel = new LinkedList<>();
