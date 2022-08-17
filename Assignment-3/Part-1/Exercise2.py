1e7d7f3678f2e9506604b094becefd603092cfc8
Arthur Viegas Eguia <95381448+Arthur-Viegas-Eguia@users.noreply.github.com>
2022-08-16 22:42:03 -0500

    Add files via upload

--- Assignment-3/Part-1/Exercise2.py
+++ Assignment-3/Part-1/Exercise2.py
@@ -1,42 +1,49 @@
 import heapq
-def parseFile():
-    fileNum = 1
-    file = open("input.txt", "r+")
-    for line in file:
-        file.truncate()
-        break
-    heap = []
-    count = 0
-    """
-    for line in file:
-        if count % 3 == 0:
-            tempFile = open("temp"+str(fileNum)+".txt", "w")
-            arr = []
-            arr.append(int(line.strip()))
-        elif count % 3 == 2:
-            arr.append(int(line.strip()))
-            arr.sort()
-            print(arr)
-            for i in arr:
-                tempFile.write(str(i)+"\n")
-            heap.append((arr[0], "temp"+str(fileNum)+".txt"))
-            fileNum += 1
-        else:
-            arr.append(int(line.strip()))
+import os
+import shutil
+def readIntegers(file, target):
+    arr = []
+    num = file.readline()
+    while num:
+        arr.append(int(num))
+        if len(arr) == target:
+            break
+        num = file.readline()
+    return arr
+def readAndSort(fileName, target):
+    count = 1
+    file = open(fileName, "r")
+    arr = readIntegers(file, target)
+    if os.path.exists('./temp/'):
+        shutil.rmtree('./temp/')
+    os.mkdir("./temp")
+    while len(arr) > 0:
+        arr.sort()
+        tempFile = open("./temp/temp"+str(count)+".txt", "w")
+        for i in arr:
+            tempFile.write(str(i)+"\n")
+        tempFile.close()
+        arr = readIntegers(file, target)
         count += 1
-    if count % 3 != 0:
-            arr.append(int(line.strip()))
-            arr.sort()
-            print(arr)
-            for i in arr:
-                tempFile.write(str(i)+"\n")
-            heap.append((arr[0], "temp"+str(fileNum)+".txt"))
-            fileNum += 1
-    
-    heapq.heapify(heap)
-    while heap:
-        
-    print(heap)
-        
-    """
-parseFile()
+    file.close()
+def heapSort():
+    file = open("output.txt","w+")
+    heap = []
+    for tempFiles in os.listdir("./temp"):
+        if os.path.isfile("./temp/"+tempFiles):
+            tempFile = open("./temp/"+str(tempFiles))
+            heap.append((int(tempFile.readline()), tempFile))
+        heapq.heapify(heap)
+    while len(heap) > 0:
+        minVal = heapq.heappop(heap)
+        file.write(str(minVal[0])+"\n")
+        num = minVal[1].readline()
+        if num:
+            heapq.heappush(heap, (int(num), minVal[1]))
+        else:
+            minVal[1].close()
+    file.close()
+def externalSort(fileName, target):
+    readAndSort(fileName, target)
+    heapSort()
+externalSort("input.txt", 100)
